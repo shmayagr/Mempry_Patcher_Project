6f704040016bbfe0d30aa48b751ae63d1436caa0
shmayagr <45229193+shmayagr@users.noreply.github.com>
2019-01-02 11:51:29 +0200

    delete files
    delete Master Branch files

--- ProjectMemoryPatcher/MemoryPatcher/MemoryPatcher/accessMem.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-//#ifndef ACC_MEM_H
-//#define ACC_MEM_H
-//#endif // !ACC_MEM_H
-char* __stdcall getFirstSectionAddress();
-
--- ProjectMemoryPatcher/injectedMethods/injectedDLL/injectedDLL/Utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<string>
-#include"stdafx.h"
-int encodeHex(std::string hexStr);
-bool patternCompare(BYTE* bytes, std::string hexStr);
-void patternSet(BYTE* dst, std::string hexStr);
